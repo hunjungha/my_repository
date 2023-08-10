cddd293c72eddeff4a87c24396e222b411e4615f
hunjungha <141399879+hunjungha@users.noreply.github.com>
2023-08-10 11:11:01 +0900

    project_1.py

--- project_1.py
+++ project_1.py
@@ -12,7 +12,7 @@ def main_window () :
     return render_template("main.html")
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="8080")
+    app.run(host="0.0.0.0", port="5000")
 
 
 
