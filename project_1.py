815d6d5607bce3c04c32b5d1be7085c66798eb64
hunjungha <141399879+hunjungha@users.noreply.github.com>
2023-08-10 15:11:06 +0900

    Add files via upload

--- project_1.py
+++ project_1.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request, make_response, jsonify
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
+
+@app.route("/", methods=['GET','POST'])
+def main_window_main () :
+    return render_template("main.html")
 
 @app.route("/평단가 및 수익률 확인", methods=['GET','POST'])
 def calculation () :
@@ -12,7 +16,4 @@ def main_window () :
     return render_template("main.html")
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="5000")
-
-
-
+    app.run(host="0.0.0.0", port="8000")
