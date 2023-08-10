a9e37e4fef812a114fe8c7f8d4402a689b2e8c3a
hunjungha <141399879+hunjungha@users.noreply.github.com>
2023-08-10 15:18:41 +0900

    Add files via upload

--- project_1.py
+++ project_1.py
@@ -16,4 +16,4 @@ def main_window () :
     return render_template("main.html")
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="8000")
+    app.run(host="0.0.0.0", port="8000", debug=True)
