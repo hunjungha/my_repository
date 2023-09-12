e24c099e990ab9a73d3c837ae0e93c44ad79c8f9
hunjungha <141399879+hunjungha@users.noreply.github.com>
2023-09-12 23:23:36 +0900

    project_1.py

--- project_1.py
+++ project_1.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, request, make_response, jsonify, send_
 
 app = Flask(__name__, static_url_path='/static')
 
+@app.route("/", methods=['GET','POST'])
+def main_window_main () :
+    return render_template("main.html")
+
 @app.route('/robots.txt')
 def robot_to_root():
     return send_from_directory(app.static_folder, 'robots.txt')
@@ -10,17 +14,10 @@ def robot_to_root():
 def site_to_root():
     return send_from_directory(app.static_folder, request.path[1:])
 
-@app.route("/", methods=['GET','POST'])
-def main_window_main () :
-    return render_template("main.html")
-
 @app.route("/평단가 및 수익률 확인", methods=['GET','POST'])
 def calculation () :
     return render_template("calculation.html")
 
-@app.route("/main", methods=['GET','POST'])
-def main_window () :
-    return render_template("main.html")
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="8080", debug=True)
+    app.run(host="0.0.0.0", port="8080")
